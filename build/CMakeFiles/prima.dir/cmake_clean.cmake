f4c3bfd48c9c772ee7a625b17051adca0ddaa303
KellSkog <kell.skog@gmail.com>
2019-12-08 09:25:16 +0100

    Use CMake from build

---
+++ build/CMakeFiles/prima.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prima.dir/src/main.cc.o"
+  "prima.pdb"
+  "prima"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prima.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
